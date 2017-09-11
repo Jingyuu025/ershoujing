2574e9544ddf4319457f81bc3432cae0ec2ba609
葛坤进 <gekunjin@nongfenqi.com>
2017-09-11 16:47:05 +0800

    -- test

---
+++ ershoujing-server/src/main/java/com/jingyuu/ershoujing/Application.java
@@ -0,0 +1,38 @@
+package com.jingyuu.ershoujing;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import java.util.Arrays;
+
+/**
+ * @author owen
+ * @date 2017-09-07
+ */
+@Slf4j
+@EnableAsync
+@SpringBootApplication
+public class Application {
+    @Resource
+    private Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void initApplication() {
+        log.info("默认环境参数：{}", Arrays.toString(environment.getDefaultProfiles()));
+    }
+
+    @PreDestroy
+    public void destroyApplication() {
+        log.info("应用关闭.");
+    }
+}
